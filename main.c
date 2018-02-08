05f3c269bd25d98fbf96bdc50c74096965726a6e
FridayFang <FridayFang@zhaoxin.com>
2018-02-08 19:35:13 +0800

    add test sample in main.c
    Signed-off-by: FridayFang <FridayFang@zhaoxin.com>

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-#include "cList.h"
 
 
 int main()
 {
+	//commonList_test1();
+	//commonList_test2();
+	//commonList_test3();
+
+	//hashTable_test2();
+	//hashTable_test3();
+	//hashTable_test4();
+
+	//maxHeap_test();
+
+	//commonTree_test1();
+	//commonTree_test2();
+
+	//skipList_test1();
+	//skipList_test2();
+	//skipList_test3();
+
+	uFindSet_test();
 	return 0;
 }
